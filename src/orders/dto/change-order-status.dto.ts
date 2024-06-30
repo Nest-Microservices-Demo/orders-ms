3cfe8726d34f8911c28fa7a5dbd4fe0c2b9197d5
SantiParra <122759810+Santiparra@users.noreply.github.com>
2024-06-30 18:39:13 -0300

    initial commit

---
+++ src/orders/dto/change-order-status.dto.ts
@@ -0,0 +1,15 @@
+import { OrderStatus } from "@prisma/client";
+import { IsEnum, IsUUID } from "class-validator";
+import { OrderStatusList } from "../enum/order.enum";
+
+export class ChangeOrderStatusDto {
+
+  @IsUUID(4)
+  id: string;
+
+  @IsEnum( OrderStatusList, {
+    message: `Invalid order status. Use one of the following: ${ OrderStatusList }`
+  } )
+  status: OrderStatus
+
+}
